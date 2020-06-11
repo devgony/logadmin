453097b819b1e0fe516ea2d4ae8a0c8bfe535e82
devgony <devgony@gmail.com>
2020-06-12 00:28:37 +0900

    prettier+headerStyle

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import {
-	BarChart
-	,Bar
-	,Line
-	,LineChart
-	,XAxis
-	,YAxis
-	,Tooltip
-} from 'recharts';
+  BarChart,
+  Bar,
+  Line,
+  LineChart,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from "recharts";
 import ReactTable from "react-table";
 import "react-table/react-table.css";
 const controller = new AbortController();
@@ -16,100 +16,123 @@ const signal = controller.signal;
 
 class App extends React.Component {
   constructor(props) {
-  	super(props);
-  	this.state = {
-		memPCT: []
-		,activeSessions: []
-  	}
+    super(props);
+    this.state = {
+      memPCT: [],
+      activeSessions: [],
+    };
   }
   componentDidMount() {
-	this.intervalIdPerf = this.fetchAndSet();
-	this.intervalIdActiveSessions = this.fetchActiveSessions();
-	console.log(this.intervalIdPerf, this.intervalIdActiveSessions)
-	}
-	componentWillUnmount() {
-		clearInterval(this.intervalIdPerf);
-		clearInterval(this.intervalIdActiveSessions);
-	}
-	fetchAndSet() {
-		return setInterval(() => {
-			fetch('/test', { signal })
-				.then(res => {
-					return res.json()
-				}).then(resObj => {
-					this.setState((state) => {
-						if (state.memPCT.length < 5) {
-							return {memPCT: [...state.memPCT, resObj]}
-						} else {
-							return {memPCT: [...state.memPCT.slice(state.memPCT.length - 4), resObj]}
-						}
-					})
-				}).catch(err => {
-					if (err.name === 'AbortError') {
-					  console.log('Fetch aborted');
-					} else {
-					  console.error('Uh oh, an error!', err);
-					}
-				});
-		}, 1000);
-	}
-	fetchActiveSessions() {
-		return setInterval(() => {
-			fetch('/active-sessions', { signal })
-				.then(res => {
-					return res.json()
-				}).then(resObj => {
-					this.setState((state) => {
-						return {activeSessions: resObj}
-					})
-				}).catch(err => {
-					if (err.name === 'AbortError') {
-					  console.log('Fetch aborted');
-					} else {
-					  console.error('Uh oh, an error!', err);
-					}
-				});
-		}, 1000);
-	}
+    this.intervalIdPerf = this.fetchAndSet();
+    this.intervalIdActiveSessions = this.fetchActiveSessions();
+    console.log(this.intervalIdPerf, this.intervalIdActiveSessions);
+  }
+  componentWillUnmount() {
+    clearInterval(this.intervalIdPerf);
+    clearInterval(this.intervalIdActiveSessions);
+  }
+  fetchAndSet() {
+    return setInterval(() => {
+      fetch("/test", { signal })
+        .then((res) => {
+          return res.json();
+        })
+        .then((resObj) => {
+          this.setState((state) => {
+            if (state.memPCT.length < 5) {
+              return { memPCT: [...state.memPCT, resObj] };
+            } else {
+              return {
+                memPCT: [
+                  ...state.memPCT.slice(state.memPCT.length - 4),
+                  resObj,
+                ],
+              };
+            }
+          });
+        })
+        .catch((err) => {
+          if (err.name === "AbortError") {
+            console.log("Fetch aborted");
+          } else {
+            console.error("Uh oh, an error!", err);
+          }
+        });
+    }, 1000);
+  }
+  fetchActiveSessions() {
+    return setInterval(() => {
+      fetch("/active-sessions", { signal })
+        .then((res) => {
+          return res.json();
+        })
+        .then((resObj) => {
+          this.setState((state) => {
+            return { activeSessions: resObj };
+          });
+        })
+        .catch((err) => {
+          if (err.name === "AbortError") {
+            console.log("Fetch aborted");
+          } else {
+            console.error("Uh oh, an error!", err);
+          }
+        });
+    }, 1000);
+  }
   render() {
     return (
       <div id="wrapper">
-	      <h1>helloReact!</h1>
-		  <LineChart width={500} height={300} data={this.state.memPCT}>
-			<XAxis dataKey="name" />
-			<YAxis />
-			<Line dataKey="value" />
-		  </LineChart>
-		  
-		  <ReactTable
-				data={this.state.activeSessions}
-				columns={[
-					{
-					columns: [
-						{Header: "SPID", accessor: "SPID"}
-						,{Header: "STATUS", accessor: "STATUS"}
-						,{Header: "DB_NAME", accessor: "DB_NAME"}
-						,{Header: "LOGINAME", accessor: "LOGINAME"}
-						,{Header: "HOSTNAME", accessor: "HOSTNAME"}
-						,{Header: "BLOCKED", accessor: "BLOCKED"}
-						,{Header: "Q.TEXT", accessor: "Q.TEXT"}
-						,{Header: "CMD", accessor: "CMD"}
-						,{Header: "CPU", accessor: "CPU"}
-						,{Header: "PHYSICAL_IO", accessor: "PHYSICAL_IO"}
-						,{Header: "LAST_BATCH", accessor: "LAST_BATCH"}
-						,{Header: "PROGRAM_NAME", accessor: "PROGRAM_NAME"}
-					]
-					}
-				]}
-				defaultPageSize={20}
-				style={{
-					height: "400px" // This will force the table body to overflow and scroll, since there is not enough room
-				}}
-				className="-striped -highlight"
-       	  />
-		  <p>activeSessions: {JSON.stringify(this.state.activeSessions)}</p>
+        <h1>helloReact!</h1>
+        <LineChart width={500} height={300} data={this.state.memPCT}>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Line dataKey="value" />
+        </LineChart>
 
-	  </div>
+        <ReactTable
+          data={this.state.activeSessions}
+          columns={[
+            {
+              columns: [
+                {
+                  Header: "SPID",
+                  accessor: "SPID",
+                  headerStyle: {
+                    background: "blue",
+                    textAlign: "center",
+                    color: "darkorange",
+                    borderRadius: "5px",
+                    padding: "5px",
+                    border: "1px solid black",
+                    borderRight: "3px solid yellow",
+                    borderLeft: "3px solid yellow",
+                    borderTop: "3px solid yellow",
+                    borderBottom: "3px solid yellow",
+                  },
+                },
+                { Header: "STATUS", accessor: "STATUS" },
+                { Header: "DB_NAME", accessor: "DB_NAME" },
+                { Header: "LOGINAME", accessor: "LOGINAME" },
+                { Header: "HOSTNAME", accessor: "HOSTNAME" },
+                { Header: "BLOCKED", accessor: "BLOCKED" },
+                { Header: "Q.TEXT", accessor: "Q.TEXT" },
+                { Header: "CMD", accessor: "CMD" },
+                { Header: "CPU", accessor: "CPU" },
+                { Header: "PHYSICAL_IO", accessor: "PHYSICAL_IO" },
+                { Header: "LAST_BATCH", accessor: "LAST_BATCH" },
+                { Header: "PROGRAM_NAME", accessor: "PROGRAM_NAME" },
+              ],
+            },
+          ]}
+          defaultPageSize={20}
+          style={{
+            height: "400px", // This will force the table body to overflow and scroll, since there is not enough room
+          }}
+          className="-striped -highlight"
+        />
+        <p>activeSessions: {JSON.stringify(this.state.activeSessions)}</p>
+      </div>
     );
   }
 }
--- src/server.js
+++ src/server.js
@@ -1,47 +1,52 @@
-const express = require('express');
-const bodyParser = require('body-parser')
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
-const sql = require('mssql');
+const sql = require("mssql");
 var config = {
-	"user": "LOGADMIN",
-	"password": "LABC123",
-	"server": "localhost"
-}
-sql.connect(config, err => { 
-    if(err){
-        throw err ;
+  user: "LOGADMIN",
+  password: "LABC123",
+  server: "localhost",
+};
+sql.connect(config, (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("Connection Successful !");
+});
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/ping", function (req, res) {
+  return res.send("pong");
+});
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public", "index.html"));
+});
+
+app.get("/test", (req, res, next) => {
+  let currentTime = new Date().toString().split(" ")[4];
+  new sql.Request().query(
+    `select cntr_value as pagelookup from sys.dm_os_performance_counters s where counter_name like 'Page lookups/sec%'`,
+    (err, result) => {
+      let pagelookup = result.recordset[0].pagelookup;
+      pagelookup = pagelookup.slice(pagelookup.length - 3);
+      res.json({ name: currentTime, value: pagelookup });
     }
-    console.log("Connection Successful !");
+  );
 });
 
-
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '../public', 'index.html'));
-});
-
-app.get('/test', (req, res, next) => {
-    let currentTime = new Date().toString().split(' ')[4]
-    new sql.Request().query(`select cntr_value as pagelookup from sys.dm_os_performance_counters s where counter_name like 'Page lookups/sec%'`, (err, result) => {
-        let pagelookup = result.recordset[0].pagelookup
-        pagelookup = pagelookup.slice(pagelookup.length-3)
-        res.json({name: currentTime, value: pagelookup});
-    })
-})
-
-app.get('/active-sessions', (req, res, next) => {
-    new sql.Request().query(`SELECT SPID, STATUS, DB_NAME(S.DBID) DB_NAME, LOGINAME, HOSTNAME, BLOCKED, Q.TEXT, CMD, CPU, PHYSICAL_IO, LAST_BATCH, PROGRAM_NAME
+app.get("/active-sessions", (req, res, next) => {
+  new sql.Request().query(
+    `SELECT SPID, STATUS, DB_NAME(S.DBID) DB_NAME, LOGINAME, HOSTNAME, BLOCKED, Q.TEXT, CMD, CPU, PHYSICAL_IO, LAST_BATCH, PROGRAM_NAME
     FROM MASTER.DBO.SYSPROCESSES S OUTER APPLY SYS.DM_EXEC_SQL_TEXT(S.SQL_HANDLE) Q
     WHERE 1=1
-    and status not in ('background','sleeping')`, (err, result) => {
-        res.json(result.recordset);
-    })
-})
+    and status not in ('background','sleeping')`,
+    (err, result) => {
+      res.json(result.recordset);
+    }
+  );
+});
 
 app.listen(process.env.PORT || 8080);
