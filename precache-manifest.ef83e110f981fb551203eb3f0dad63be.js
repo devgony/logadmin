22e5ad988e9e6cc9872f8bbacbad29013dd47862
devgony <devgony@gmail.com>
2020-09-21 22:17:07 +0900

    Updates

---
+++ precache-manifest.ef83e110f981fb551203eb3f0dad63be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65946c8841fbbe04dc73b688f6948693",
+    "url": "/cherish/index.html"
+  },
+  {
+    "revision": "617cc5b2b127af2addd5",
+    "url": "/cherish/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "3e97c097ad12e8fe4fed",
+    "url": "/cherish/static/js/2.08c2e592.chunk.js"
+  },
+  {
+    "revision": "998fa3a8f95293ea95ee290688877c4d",
+    "url": "/cherish/static/js/2.08c2e592.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "617cc5b2b127af2addd5",
+    "url": "/cherish/static/js/main.e66e164a.chunk.js"
+  },
+  {
+    "revision": "ab3c2510238e12bef14a",
+    "url": "/cherish/static/js/runtime-main.8c9fe7c6.js"
+  }
+]);
